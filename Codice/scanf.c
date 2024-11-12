ea7bcc5f23e3a12e0331e908c7a6a70904cb0189
Antonio <poddaa18@gmail.com>
2024-11-12 16:07:17 +0100

    up

---
+++ Codice/scanf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int primo;
+    int secondo;
+    int risultato;
+
+    printf("\nInserisci primo numero:");
+    scanf("%d", &primo);
+
+    printf("\nInserisci il secondo numero: ");
+    scanf("%d", &secondo);
+
+   risultato = primo + secondo;
+
+   printf("\n %d + %d =%d\n",primo, secondo,  risultato);
+   printf("\n %d - %d =%d\n",primo, secondo,  primo - secondo);
+   printf("\n %d * %d =%d\n",primo, secondo,  primo * secondo);
+   printf("\n%d / %d =%d\n",primo, secondo,  primo / secondo);
+   printf("\n / %d diviso %d ha resto %d\n",primo, secondo,  primo % secondo);
+
+
+    return 0;
+}
---
+++ Codice/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+*Questo è il mio codice
+*/
+
+// anche questo è un commento
+
+int main()
+{
+    printf("\nCiao mondo!\n");
+    return 0;
+}
